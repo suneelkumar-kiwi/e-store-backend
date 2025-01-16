0f80d8e3eadb51a934668a4408ea0a8590de0234
suneelpondric <suneelpondric999@gmail.com>
2025-01-16 11:18:49 +0530

    Add host port

--- server.js
+++ server.js
@@ -23,4 +23,7 @@ app.use('/api/', authRoutes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const HOST = process.env.HOST || '0.0.0.0';
+app.listen(PORT, HOST, () => {
+    console.log(`Server running at http://${HOST}:${PORT}`);
+});
